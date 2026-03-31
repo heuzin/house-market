4f9ae081ad8c42010a55f071e5a25d5b97d477cf
Matheus Silva <msilvaqs@outlook.com>
2026-03-31 10:48:50 -0400

    Google OAuth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import OAuth from "../components/OAuth";
 
 const SignIn = () => {
   const navigate = useNavigate();
@@ -89,7 +90,7 @@ const SignIn = () => {
         </div>
       </form>
 
-      {/* Google OAuth */}
+      <OAuth />
 
       <Link to="/sign-up" className="registerLink">
         Sign Up Instead
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,6 +10,7 @@ import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import OAuth from "../components/OAuth";
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -114,7 +115,7 @@ const SignUp = () => {
         </div>
       </form>
 
-      {/* Google OAuth */}
+      <OAuth />
 
       <Link to="/sign-in" className="registerLink">
         Sign In Instead
